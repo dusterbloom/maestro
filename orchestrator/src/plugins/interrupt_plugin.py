2aefac532573765d330557a4d8bb2f4ed59f6bf5
Peppi Littera <giuseppe.littera@gmail.com>
2025-07-18 18:28:56 +0200

    Auto-commit 1752856136

--- orchestrator/src/plugins/interrupt_plugin.py
+++ orchestrator/src/plugins/interrupt_plugin.py
@@ -7,7 +7,7 @@ import asyncio
 import time
 import logging
 from typing import Dict, Any, Optional
-from .base_plugin import BasePlugin, PluginConfig
+from plugins.base_plugin import BasePlugin, PluginConfig
 
 logger = logging.getLogger(__name__)
 
