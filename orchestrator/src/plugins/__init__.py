f6e02d7a5abe355b2d7a7f96af4b30f05b781701
Peppi Littera <giuseppe.littera@gmail.com>
2025-07-18 18:52:46 +0200

    Auto-commit 1752857566

--- orchestrator/src/plugins/__init__.py
+++ orchestrator/src/plugins/__init__.py
@@ -1,8 +1,8 @@
 """Plugin system for orchestrator parallel processing."""
 
-from .base_plugin import BasePlugin, PluginManager
-from .memory_plugin import MemoryPlugin
-from .speaker_plugin import SpeakerPlugin
-from .interrupt_plugin import InterruptPlugin
+from plugins.base_plugin import BasePlugin, PluginManager
+from plugins.memory_plugin import MemoryPlugin
+from plugins.speaker_plugin import SpeakerPlugin
+from plugins.interrupt_plugin import InterruptPlugin
 
 __all__ = ['BasePlugin', 'PluginManager', 'MemoryPlugin', 'SpeakerPlugin', 'InterruptPlugin']
